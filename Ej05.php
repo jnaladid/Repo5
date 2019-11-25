3704f41e1b43bc5bc4fdc91e1b45d63b308bcbdc
Joan <Joan@DESKTOP-MSRTH4Q>
2019-11-25 09:21:06 +0100

    Subida de tamaño

---
+++ Ej05.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $N = 1;
+        $A = 1.1;
+        $C = 'U';
+
+                
+        echo "$N <br> $A <br> $C";
+        echo "<br>";
+        echo $N + $A;
+        echo "<br>";
+        echo $N - $A;
+        echo "<br>";
+        echo ord($C);
+        ?>
+    </body>
+</html>
